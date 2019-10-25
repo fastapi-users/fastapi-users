9b336bdda185d6076333336512bb186ffe1f194e
frankie567 <frankie567@users.noreply.github.com>
2019-10-25 06:35:09 +0000

    Automated deployment: Fri Oct 25 06:35:09 UTC 2019 8987b1bb2a165dae2a3f8318b524d5a65e20b4a3

--- src/full_sqlalchemy.py
+++ src/full_sqlalchemy.py
@@ -35,14 +35,19 @@ class User(BaseUser):
 
 auth = JWTAuthentication(secret=SECRET, lifetime_seconds=3600)
 
-
-def on_after_forgot_password(user, token):
-    print(f"User {user.id} has forgot their password. Reset token: {token}")
-
-
 app = FastAPI()
-fastapi_users = FastAPIUsers(user_db, auth, User, on_after_forgot_password, SECRET)
+fastapi_users = FastAPIUsers(user_db, auth, User, SECRET)
 app.include_router(fastapi_users.router, prefix="/users", tags=["users"])
+
+
+@fastapi_users.on_after_register()
+def on_after_register(user: User):
+    print(f"User {user.id} has registered.")
+
+
+@fastapi_users.on_after_forgot_password()
+def on_after_forgot_password(user: User, token: str):
+    print(f"User {user.id} has forgot their password. Reset token: {token}")
 
 
 @app.on_event("startup")
