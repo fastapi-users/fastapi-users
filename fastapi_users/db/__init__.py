60c815e4257ab73744e04ea66264207b2d4d79d0
François Voron <fvoron@kpi-intelligence.com>
2019-10-31 21:43:10 +0100

    Fix #35: catch import error when not having optional DB depedencies

--- fastapi_users/db/__init__.py
+++ fastapi_users/db/__init__.py
@@ -1,6 +1,14 @@
 from fastapi_users.db.base import BaseUserDatabase  # noqa: F401
-from fastapi_users.db.mongodb import MongoDBUserDatabase  # noqa: F401
-from fastapi_users.db.sqlalchemy import (  # noqa: F401
-    SQLAlchemyBaseUserTable,
-    SQLAlchemyUserDatabase,
-)
+
+try:
+    from fastapi_users.db.mongodb import MongoDBUserDatabase  # noqa: F401
+except ImportError:  # pragma: no cover
+    pass
+
+try:
+    from fastapi_users.db.sqlalchemy import (  # noqa: F401
+        SQLAlchemyBaseUserTable,
+        SQLAlchemyUserDatabase,
+    )
+except ImportError:  # pragma: no cover
+    pass
