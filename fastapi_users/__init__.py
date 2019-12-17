935bccf528a6ffd3c90a2be02b28ee7945e0c436
François Voron <fvoron@kpi-intelligence.com>
2019-12-17 15:50:12 +0100

    Bump version: 0.4.0 → 0.4.1

--- fastapi_users/__init__.py
+++ fastapi_users/__init__.py
@@ -1,6 +1,6 @@
 """Ready-to-use and customizable users management for FastAPI."""
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 from fastapi_users.fastapi_users import FastAPIUsers  # noqa: F401
 from fastapi_users.models import BaseUser  # noqa: F401
