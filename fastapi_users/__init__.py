985602fbd1643f6b0671d11d0b382c3e46f68745
François Voron <fvoron@gmail.com>
2020-04-09 17:14:04 +0200

    Bump version: 0.7.2 → 0.7.3

--- fastapi_users/__init__.py
+++ fastapi_users/__init__.py
@@ -1,6 +1,6 @@
 """Ready-to-use and customizable users management for FastAPI."""
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 from fastapi_users import models  # noqa: F401
 from fastapi_users.fastapi_users import FastAPIUsers  # noqa: F401
