aac96a868ed189f4b000252a3599102be485c4b7
Artem Revenko <artreven@gmail.com>
2021-11-03 16:35:34 +0100

    Do not allow setting scopes during registration

--- fastapi_users/models.py
+++ fastapi_users/models.py
@@ -14,6 +14,7 @@ class CreateUpdateDictModel(BaseModel):
                 "is_active",
                 "is_verified",
                 "oauth_accounts",
+                "scopes"
             },
         )
 
