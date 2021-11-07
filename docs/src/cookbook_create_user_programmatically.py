b80061bbc4fe828f330f663f411504719c370916
François Voron <fvoron@gmail.com>
2021-11-07 11:32:22 +0100

    Fix #788: add cookbook to create user programmatically

---
+++ docs/src/cookbook_create_user_programmatically.py
@@ -0,0 +1,24 @@
+import contextlib
+
+from fastapi_users.manager import UserAlreadyExists
+
+from app.db import get_user_db
+from app.models import UserCreate
+from app.users import get_user_manager
+
+get_user_db_context = contextlib.asynccontextmanager(get_user_db)
+get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
+
+
+async def create_user(email: str, password: str, is_superuser: bool = False):
+    try:
+        async with get_user_db_context() as user_db:
+            async with get_user_manager_context(user_db) as user_manager:
+                user = await user_manager.create(
+                    UserCreate(
+                        email=email, password=password, is_superuser=is_superuser
+                    )
+                )
+                print(f"User created {user}")
+    except UserAlreadyExists:
+        print(f"User {email} already exists")
