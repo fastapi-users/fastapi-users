dc15309b09b2756ebcbb08e7df2eedbe51629688
François Voron <fvoron@gmail.com>
2021-12-30 15:47:31 +0100

    Fix ImportError when redis optional dependency not installed

--- fastapi_users/authentication/__init__.py
+++ fastapi_users/authentication/__init__.py
@@ -1,6 +1,12 @@
 from fastapi_users.authentication.authenticator import Authenticator
 from fastapi_users.authentication.backend import AuthenticationBackend
-from fastapi_users.authentication.strategy import JWTStrategy, RedisStrategy, Strategy
+from fastapi_users.authentication.strategy import JWTStrategy, Strategy
+
+try:
+    from fastapi_users.authentication.strategy import RedisStrategy
+except ImportError:  # pragma: no cover
+    pass
+
 from fastapi_users.authentication.transport import (
     BearerTransport,
     CookieTransport,
--- fastapi_users/authentication/strategy/__init__.py
+++ fastapi_users/authentication/strategy/__init__.py
@@ -3,7 +3,11 @@ from fastapi_users.authentication.strategy.base import (
     StrategyDestroyNotSupportedError,
 )
 from fastapi_users.authentication.strategy.jwt import JWTStrategy
-from fastapi_users.authentication.strategy.redis import RedisStrategy
+
+try:
+    from fastapi_users.authentication.strategy.redis import RedisStrategy
+except ImportError:  # pragma: no cover
+    pass
 
 __all__ = [
     "JWTStrategy",
