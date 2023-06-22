0bfd8cc5e5c926cfff94a86ceeabb3580fdf3012
Justin <justinckl@outlook.com>
2023-06-22 12:57:50 +0530

    add AsyncIterator to the DependencyCallable type

--- fastapi_users/types.py
+++ fastapi_users/types.py
@@ -1,4 +1,4 @@
-from typing import AsyncGenerator, Callable, Coroutine, Generator, TypeVar, Union
+from typing import AsyncGenerator, AsyncIterator, Callable, Coroutine, Generator, TypeVar, Union
 
 RETURN_TYPE = TypeVar("RETURN_TYPE")
 
@@ -9,5 +9,6 @@ DependencyCallable = Callable[
         Coroutine[None, None, RETURN_TYPE],
         AsyncGenerator[RETURN_TYPE, None],
         Generator[RETURN_TYPE, None, None],
+        AsyncIterator[RETURN_TYPE],
     ],
 ]
