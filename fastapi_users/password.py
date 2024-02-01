d0ba6f09b353212367b5f7431af3e56944bbfa4d
Jimmy Angel <jimmy@generalsoftwareinc.com>
2024-02-01 16:02:59 -0500

    Temporarily add bcrypt in a straightforward way

--- fastapi_users/password.py
+++ fastapi_users/password.py
@@ -1,36 +1,40 @@
-from typing import Optional, Protocol, Tuple
+import secrets
+from typing import Protocol, Tuple
 
-from passlib import pwd
-from passlib.context import CryptContext
+import bcrypt
 
 
 class PasswordHelperProtocol(Protocol):
     def verify_and_update(
         self, plain_password: str, hashed_password: str
-    ) -> Tuple[bool, str]:
-        ...  # pragma: no cover
+    ) -> Tuple[bool, str]: ...  # pragma: no cover
 
-    def hash(self, password: str) -> str:
-        ...  # pragma: no cover
+    def hash(self, password: str) -> str: ...  # pragma: no cover
 
-    def generate(self) -> str:
-        ...  # pragma: no cover
+    def generate(self) -> str: ...  # pragma: no cover
 
 
 class PasswordHelper(PasswordHelperProtocol):
-    def __init__(self, context: Optional[CryptContext] = None) -> None:
-        if context is None:
-            self.context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-        else:
-            self.context = context  # pragma: no cover
+    def __init__(self, salt_rounds: int = 12) -> None:
+        self.salt_rounds = salt_rounds
 
     def verify_and_update(
         self, plain_password: str, hashed_password: str
     ) -> Tuple[bool, str]:
-        return self.context.verify_and_update(plain_password, hashed_password)
+        hashed_bytes = hashed_password.encode("utf-8")
+        plain_bytes = plain_password.encode("utf-8")
+
+        if bcrypt.checkpw(plain_bytes, hashed_bytes):
+            return True, hashed_password
+        else:
+            new_hashed_password = self.hash(plain_password)
+            return False, new_hashed_password
 
     def hash(self, password: str) -> str:
-        return self.context.hash(password)
+        hashed_bytes = bcrypt.hashpw(
+            password.encode("utf-8"), bcrypt.gensalt(self.salt_rounds)
+        )
+        return hashed_bytes.decode("utf-8")
 
     def generate(self) -> str:
-        return pwd.genword()
+        return secrets.token_urlsafe()
