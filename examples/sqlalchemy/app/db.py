ddff3e96771afc6daaaa1a3aedb86915c80b38f1
George Tian <me@georgetian.com>
2025-01-02 18:32:12 +1100

    Update import in `db.py` as well

--- examples/sqlalchemy/app/db.py
+++ examples/sqlalchemy/app/db.py
@@ -1,7 +1,7 @@
 from collections.abc import AsyncGenerator
 
 from fastapi import Depends
-from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
